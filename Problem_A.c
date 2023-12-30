4f6a6c57629c5c0a39bade031a925e43d89461e3
ChristosKyrgios <153856301+ChristosKyrgios@users.noreply.github.com>
2023-12-30 20:15:36 +0200

    Update Problem_A.c

--- Problem_A.c
+++ Problem_A.c
@@ -3,17 +3,17 @@
 #include <string.h>
 
 /* Η συνάρτηση δεν θα μας επιστρέψει κάποια τιμή. Γι'αυτό το λόγο ξεκινά με το void. Το όνομα της είναι generateReverseComplementary και στις παραμέτρους ορίζονται οι δυο μεταβλητές τύπου string, δηλαδή η original και η reverseComplementary. Παρακάτω θα δούμε ότι στη θέση τους θα μπουν οι μεταβλητές seq και reverseComplementarySeq αντίστοιχα */
-   void generateReverseComplementary(char original[], char reverseComplementary[]) 
+void generateReverseComplementary(char original[], char reverseComplementary[]) 
 {
     int i;
     int length = strlen(original);
     
     for (i = 0; i < length; i++) 
     {
-        switch (original[i]) 
+        switch (original[i]) /* Στη θέση πολλαπλών if-else χρησιμοποιείται η συνάρτηση switch. Αναζητώντας χαρακτήρα-χαρακτήρα στο string original (ή seq) όταν ισχύει η τιμή που υπάρχει στο case εκτελείται ο κώδικας που ακολουθεί και η εκτέλεση ολοκληρώνεται με το break. Τέλος, με το default εξασφαλίζεται πως κάθε άλλος χαρακτήρας πέραν αυτών που μαςενδιαφέρουν δεν θα αλλάξει */
         {
             case 'A':
-                reverseComplementary[length - i - 1] = 'T';
+                reverseComplementary[length - i - 1] = 'T'; /* Το (length - i - 1) εξασφαλίζει ότι η συμπληρωματική αλληλουχία θα αναστραφεί επίσης, ώστε να διαβαστεί για την εύρεση ενός ORF με κατεύθυνση 5' -> 3' */
                 break;
             case 'T':
                 reverseComplementary[length - i - 1] = 'A';
@@ -25,7 +25,7 @@
                 reverseComplementary[length - i - 1] = 'C';
                 break;
             default:
-                reverseComplementary[length - i - 1] = original[i];  // For any other character (e.g., N), keep it unchanged
+                reverseComplementary[length - i - 1] = original[i];  
         }
     }
     reverseComplementary[length] = '\0';  // Add null terminator at the end
@@ -38,13 +38,14 @@ int main()
 {
   
   char seq[1000];    /* Δηλώνω μεταβλητή τύπου string με μέγιστο μήκος τους 1000 χαρακτήρες, στην οποία θα αποθηκευτεί η αλληλουχία DNA */
+  char reverseComplementarySeq[1000]; /* Δηλώνω μεταβλητή που θα περιέχει τη συμπληρωματική αλληλουχία ανεστραμμένη */
   int i, j, length, count, minAminoAcids;  /* Δηλώνω μεταβλητές τύπου ακεραίου : για την αρχικοποίηση των επερχόμενων for (i και j), για το μήκος της αλληλουχίας (length), για τον μετρητή count που καθορίζει άμεσα το αποτέλεσμα (το αν μία αλληλουχία έχει ORF ή όχι) και το minAminoAcids ώστε να ελεγχθεί και ότι το μήκος του ORF είναι αρκετά μεγάλο ώστε αυτό να θεωρείται ένα έκγυρο ORF */ 
 
 while ( scanf("%s", seq ) == 1 )  /* Με την εντολή αυτή το πρόγραμμα διαβάζει και εκτελεί όσα υπάρχουν μέσα στην while για πολλές αλληλουχίες που μπορεί να πληκτρολογήσει ο χρήστης */
   {
-      length = strlen(seq);
-      count = 0;
-      minAminoAcids = 0;
+         length = strlen(seq);
+         count = 0;
+         minAminoAcids = 0;
       
       for ( i = 0 ; i < length ; i++ )  /* Με αυτή την πρώτη for, το string διαβάζεται χαρακτήρα-χαρακτήρα μέχρις ότου βρεθεί το κωδικόνιο έναρξης, κάτι που διασφαλίζεται από την if που ακολουθεί. Επειδή η πρώτη θέση στο string είναι αριθμημένη με 0 και όχι 1, για αυτό και για να γίνει αναζήτηση σε όλη την αλληλουχία η for συνεχίζεται με βήμα 1 μέχρι το length-1 και όχι μέχρι το length ( i < length ) */
             {
@@ -63,16 +64,39 @@ while ( scanf("%s", seq ) == 1 )  /* Με την εντολή αυτή το πρ
                                     break; /* Το break εξασφαλίζει ότι αφού βρεθεί ένα ATG, δεν θα ξεκινήσει η αναζήτηση εκ νέου αν μέσα στο ORF υπάρχουν και άλλα κωδικόνια ATG */
                             }
             }
+
+      generateReverseComplementary(seq, reverseComplementarySeq);
+      for ( i = 0 ; i < length ; i++ ) 
+            {
+                    if ( reverseComplementarySeq[i] == 'A' && reverseComplementarySeq[i+1] == 'T' && reverseComplementarySeq[i+2] == 'G' )
+                            {
+                                    for ( j = i + 3 ; j < length ; j += 3 )
+                                              {
+                                                      if ( (reverseComplementarySeq[j] == 'T' && reverseComplementarySeq[j+1] == 'A' && reverseComplementarySeq[j+2] == 'A') || (reverseComplementarySeq[j] == 'T' && reverseComplementarySeq[j+1] == 'A' && reverseComplementarySeq[j+2] == 'G') || (reverseComplementarySeq[j] == 'T' && reverseComplementarySeq[j+1] == 'G' && reverseComplementarySeq[j+2] == 'A') )
+                                                            { 
+                                                                  count++; /* */
+                                                                  minAminoAcids = (j - i) / 3 ;
+                                                            }
+                                              }
+                                    if ( minAminoAcids > 100 )
+                                              { count++; }
+                                    break; 
+                            }
+            }
+
+
+     
       if ( count == 0 ) /* Στις ακόλουθες if, ο μετρτής count ανάλογα την τιμή που έχει λάβει μετά την αναζήτηση, μπορεί να δώσει τρία πιθανά αποτελέσματα : α) Δεν υπάρχει ORF στην αλληλουχία όταν count = 0, β) Υπάρχει ORF στην αλληλουχία αλλά δεν έχει επαρκώς μεγάλο αριθμό αμινοξέων όταν count = 2 και γ) Υπάρχει ORF όταν count = 2 */
           { printf ("This sequence does not have an ORF\n"); }
       else if ( count == 1 )
-          { printf ("This sequence does not have a large enough ORF (with more than 5 aminoacids\n"); }
+          { printf ("This sequence does not have a large enough ORF (with more than 100 aminoacids\n"); }
       else if ( count == 2 )
           { printf ("This sequence has a valid ORF\n"); }
+    
  }
 
 return 0;
-
+   
 }
 
 
